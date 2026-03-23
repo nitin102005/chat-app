1560fccd2ca4d1df32b1c739fabb2d3903906662
Nitin Singh Pokhariya <168846806+nitin102005@users.noreply.github.com>
2026-03-23 23:51:28 +0530

    Update utils.js

--- backend/src/lib/utils.js
+++ backend/src/lib/utils.js
@@ -12,11 +12,11 @@ export const generateToken = (userId, res) => {
   });
 
   res.cookie("jwt", token, {
-    maxAge: 7 * 24 * 60 * 60 * 1000, // MS
-    httpOnly: true, // prevent XSS attacks: cross-site scripting
-    sameSite: "strict", // CSRF attacks
-    secure: process.env.NODE_ENV === "development" ? false : true,
-  });
+  maxAge: 7 * 24 * 60 * 60 * 1000,
+  httpOnly: true,
+  secure: process.env.NODE_ENV !== "development",
+  sameSite: "none", // ✅ REQUIRED for cross-domain
+});
 
   return token;
 };
