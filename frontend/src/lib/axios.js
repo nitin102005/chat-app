e82f9812299eac9d1b2efb702a51a9abd6a8c996
Nitin Singh Pokhariya <168846806+nitin102005@users.noreply.github.com>
2026-03-23 23:11:19 +0530

    Update axios.js

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -3,6 +3,6 @@ import axios from "axios";
 export const axiosInstance = axios.create({
   baseURL: import.meta.env.MODE === "development"
     ? "http://localhost:3000/api"
-    : "https://chat-app-n70t.onrender.com",
+    : "https://chat-app-n70t.onrender.com/api",
   withCredentials: true,
 });
