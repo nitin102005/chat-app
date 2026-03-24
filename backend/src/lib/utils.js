1c2bee2271838ed69355cb6ae2841a1de33fcaa0
Nitin Singh Pokhariya <168846806+nitin102005@users.noreply.github.com>
2026-03-24 18:44:02 +0530

    Update utils.js

--- backend/src/lib/utils.js
+++ backend/src/lib/utils.js
@@ -1,5 +1,5 @@
 import jwt from "jsonwebtoken";
-// import { ENV } from "./env.js";
+import { ENV } from "./env.js";
 
 export const generateToken = (userId, res) => {
 //   const { JWT_SECRET } = ENV;
@@ -14,7 +14,7 @@ export const generateToken = (userId, res) => {
   res.cookie("jwt", token, {
   maxAge: 7 * 24 * 60 * 60 * 1000,
   httpOnly: true,
-  secure: process.env.NODE_ENV !== "development",
+  secure: true, 
   sameSite: "none", // ✅ REQUIRED for cross-domain
 });
 
