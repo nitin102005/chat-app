72fd74956703297e3ba820c11c4acc6bcc0b47c2
Nitin Singh Pokhariya <168846806+nitin102005@users.noreply.github.com>
2026-03-24 19:11:25 +0530

    Update utils.js

--- backend/src/lib/utils.js
+++ backend/src/lib/utils.js
@@ -2,14 +2,15 @@ import jwt from "jsonwebtoken";
 import { ENV } from "./env.js";
 
 export const generateToken = (userId, res) => {
-//   const { JWT_SECRET } = ENV;
-//   if (!JWT_SECRET) {
-//     throw new Error("JWT_SECRET is not configured");
-//   }
+  const { JWT_SECRET, NODE_ENV } = ENV;
+  if (!JWT_SECRET) {
+    throw new Error("JWT_SECRET is not configured");
+  }
 
-  const token = jwt.sign({ userId },process.env.JWT_SECRET, {
+  const token = jwt.sign({ userId }, JWT_SECRET, {
     expiresIn: "7d",
   });
+
 
   res.cookie("jwt", token, {
   maxAge: 7 * 24 * 60 * 60 * 1000,
