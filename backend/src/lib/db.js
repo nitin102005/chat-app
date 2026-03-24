a781764f29249d18372362c565d0ab1fdf8f6d1a
Nitin Singh Pokhariya <168846806+nitin102005@users.noreply.github.com>
2026-03-24 19:02:19 +0530

    Update db.js

--- backend/src/lib/db.js
+++ backend/src/lib/db.js
@@ -2,12 +2,7 @@ import mongoose from "mongoose";
 
 export const connectDB = async () => {
   try {
-    mongoose.set("strictQuery", true);
-
-    const conn = await mongoose.connect(process.env.MONGO_URI, {
-      dbName: "chatapp",
-    });
-
+    const conn = await mongoose.connect(process.env.MONGO_URI);
     console.log(`MongoDB Connected: ${conn.connection.host}`);
   } catch (error) {
     console.error("MongoDB connection failed:", error.message);
